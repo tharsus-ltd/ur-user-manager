efa0315e74f804912b41835d7e0bdc39fea9cfbd
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2021-09-23 09:34:02 +0100

    use same exchange

--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,11 @@
 import os
 import aioredis
 
-from aio_pika import connect_robust, Message
+from aio_pika import connect_robust, Message, ExchangeType
 
 from app.singleton import Singleton
 
-REDIS_SERVICE = os.environ.get("REDIS_SERVICE","user_man_db")
+REDIS_SERVICE = os.environ.get("REDIS_SERVICE", "user_man_db")
 
 
 class Handlers(metaclass=Singleton):
@@ -14,9 +14,10 @@ class Handlers(metaclass=Singleton):
         self.redis = aioredis.from_url(f"redis://{REDIS_SERVICE}", encoding="utf-8", decode_responses=True)
         self.rabbitmq = await connect_robust("amqp://guest:guest@rabbitmq/")
         self.channel = await self.rabbitmq.channel()
+        self.exchange = await self.channel.declare_exchange("micro-rockets", ExchangeType.TOPIC, durable=True)
 
     async def send_msg(self, msg: str, topic: str):
-        await self.channel.default_exchange.publish(
+        await self.exchange.publish(
             Message(body=msg.encode()),
             routing_key=topic,
         )
